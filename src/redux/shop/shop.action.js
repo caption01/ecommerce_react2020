12426f91e7ff0ece2cf03f3278fbb6bd35d552f5
nutchapon <nutchapon.han@gmail.com>
2019-12-07 09:12:06 +0700

    commit change before convert app with redux-saga

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,7 +1,30 @@
 import ShopActionType from './shop.type';
+import { firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
 
-
-export const updateCollections = (collectionMap) => ({
-    type: ShopActionType.UPDATE_COLLECTIONS,
-    payload: collectionMap
+export const fetchCollectionsStart = () => ({
+    type: ShopActionType.FETCH_COLLECTIONS_START,
 })
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+    type: ShopActionType.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = (errMessage) => ({
+    type: ShopActionType.FETCH_COLLECTIONS_FAILURE,
+    payload: errMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+
+        const collectionRef = firestore.collection('collections')
+        dispatch(fetchCollectionsStart())
+
+        collectionRef.get()
+            .then(snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+                dispatch(fetchCollectionsSuccess(collectionsMap))
+            }).catch(err => dispatch(fetchCollectionsFailure(err.message)))
+    }
+}
